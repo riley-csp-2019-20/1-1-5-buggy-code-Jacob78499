6d032774c19d376eafbe95ea8e7ddef8b5126938
Jacob78499 <55500799+Jacob78499@users.noreply.github.com>
2019-09-18 11:33:10 -0400

    version 2

--- 115Jacobjaniak.py
+++ 115Jacobjaniak.py
@@ -4,16 +4,17 @@ import turtle as trtl
 num_turtle = trtl.Turtle()
 num_turtle.pensize(40)
 num_turtle.circle(20)
-w = 6
-y = 70
-z = 380 / w
+num_leg = 6
+leg_length = 70
+leg_placement = 360 / num_leg
 num_turtle.pensize(5)
-n = 0
-while (n < w):
+leg_count = 0
+while (leg_count < num_leg):
+  print("leg_placement*leg_count=", leg_placement*leg_count)
   num_turtle.goto(0,0)
-  num_turtle.setheading(z*n)
-  num_turtle.forward(y)
-  n = n + 1
+  num_turtle.setheading(leg_placement*leg_count)
+  num_turtle.forward(leg_length)
+  leg_count = leg_count + 1
 num_turtle.hideturtle()
 wn = trtl.Screen()
 wn.mainloop()
